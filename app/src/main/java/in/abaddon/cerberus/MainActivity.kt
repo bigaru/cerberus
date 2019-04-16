23f81b16f2e5ab5a94c80740e06875da3f60108e
BigAru <arooran@indikon.ch>
2019-04-16 14:48:54 +0200

    add recyclerview

--- app/src/main/java/in/abaddon/cerberus/MainActivity.kt
+++ app/src/main/java/in/abaddon/cerberus/MainActivity.kt
@@ -1,15 +1,29 @@
 package `in`.abaddon.cerberus
 
-import `in`.abaddon.cerberus.models.Reading
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val r = Reading(4, null, 4.0, "")
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = RecyclerAdapter(arrayOf("Meow", "Moo", "Pikachu","DRagon", "eMoo", "ePikeachu","DeeRagon"))
+
+        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+
     }
 }
---
+++ app/src/main/java/in/abaddon/cerberus/RecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package `in`.abaddon.cerberus
+
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+
+class RecyclerAdapter(private val data: Array<String>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(rowView: CardView) : RecyclerView.ViewHolder(rowView) {
+        val title = rowView.findViewById<TextView>(R.id.row_title);
+        val subtitle = rowView.findViewById<TextView>(R.id.row_subtitle);
+        val footerLeft = rowView.findViewById<TextView>(R.id.row_footer_left);
+        val footerRight = rowView.findViewById<TextView>(R.id.row_footer_right);
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val rowView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row_item, parent, false) as CardView
+
+        return ViewHolder(rowView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = data[position]
+        holder.subtitle.text = data[position] + 0
+        holder.footerLeft.text = data[position] + 1
+        holder.footerRight.text = data[position] + 2
+    }
+
+    override fun getItemCount() = data.size
+}
