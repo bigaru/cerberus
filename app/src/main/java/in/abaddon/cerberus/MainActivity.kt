b47285f1f3df204ec327c8f2f8d41c7d5db65e5d
BigAru <arooran@indikon.ch>
2019-04-16 16:07:46 +0200

    hide fab when scrolling down

--- app/src/main/java/in/abaddon/cerberus/MainActivity.kt
+++ app/src/main/java/in/abaddon/cerberus/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
@@ -18,12 +18,23 @@ class MainActivity : AppCompatActivity() {
         viewManager = LinearLayoutManager(this)
         viewAdapter = RecyclerAdapter(arrayOf("Meow", "Moo", "Pikachu","DRagon", "eMoo", "ePikeachu","DeeRagon"))
 
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+        recycler_view.apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
 
+        recycler_view.addOnScrollListener(onScrollListener)
 
+    }
+
+    private val onScrollListener = object: RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            if(dy < 0) // going up
+                fab.show()
+
+            if(dy > 0) // going down
+                fab.hide()
+        }
     }
 }
